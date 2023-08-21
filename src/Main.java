bc9000e4403576df6ee5360f7a1f133e643806ed
Adebayour66265 <adebayour66265@gmail.com>
2023-08-21 18:31:39 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+     // Mustapha.must();
+//            String myName = "nurudeen";
+//            String myNamec = myName;
+//            System.out.println(myNamec);
+//            System.out.println(myName);
+
+//        byte b1 = -128;
+//        short s1 = 20;
+//        int i1 = 100;
+//        long l1 = 999L;
+//
+//        System.out.println(b1 - i1);
+//        System.out.println(s1 - b1);
+//        System.out.println(i1 - l1);
+//        System.out.println(l1);
+
+//        Integer
+        System.out.println(Integer.MAX_VALUE);
+
+//        Short
+        System.out.println(Integer.MIN_VALUE);
+    }
+}
---
+++ src/Mustapha.java
@@ -0,0 +1,12 @@
+public class Mustapha {
+    public static void must() {
+        System.out.println("Page 1") ;
+         adeBay();
+
+       }
+
+    private static void adeBay() {
+        System.out.println("Page 2 ");
+
+    }
+}
